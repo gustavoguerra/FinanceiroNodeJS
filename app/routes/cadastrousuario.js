719610c9a044cc01c2702aafdbf7df38427123a2
gustavoguerra <37709303+gustavoguerra@users.noreply.github.com>
2019-02-12 08:46:34 -0200

    Update cadastrousuario.js

--- app/routes/cadastrousuario.js
+++ app/routes/cadastrousuario.js
@@ -14,18 +14,6 @@ module.exports = function (app) {
                 console.log(err);
                 res.render("cadastro/cadastrousuario", { result: recordset });
             });
-        });
-        
-        // var sql = require("mssql");
-        // sql.connect(app.config.dbconection, function (err) {
-
-        //     var request = new sql.Request();
-        //     request.query('select * from Usuario', function (err, recordset) {
-        //         res.send(recordset);
-        //         console.log(recordset);
-        //     });
-        // });
-
-       // res.render("cadastro/cadastrousuario");
+        });        
     });
 }
